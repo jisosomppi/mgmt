62b01b327e3c505b934e90774d078a586325edb6
Jussi Isosomppi <jisosomppi@users.noreply.github.com>
2018-04-04 08:03:54 +0000

    Create startup.sh

---
+++ startup.sh
@@ -0,0 +1,15 @@
+setxkbmap fi
+sudo apt-get update
+sudo apt-get install -y git puppet tree htop
+
+cd
+
+git clone https://github.com/jisosomppi/mgmt
+
+git config --global user.name "Jussi Isosomppi"
+git config --global user.email "jussi.isosomppi@gmail.com"
+git config --global credential.helper "cache --timeout=3600"
+git config --global push.default simple
+
+sudo cp mgmt/gitup /usr/local/bin/
+sudo chmod 755 /usr/local/bin/gitup
