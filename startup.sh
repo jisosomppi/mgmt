6ffa514fe9cfc7432897b052b77d1353451e4f85
Jussi Isosomppi <jussi.isosomppi@gmail.com>
2018-04-09 23:46:27 +0300

    updated

--- startup.sh
+++ startup.sh
@@ -1,8 +1,11 @@
+# Setup computer
+
 setxkbmap fi
 sudo apt-get update
 sudo apt-get install -y git tree htop salt-minion salt-master
-
 cd
+
+# Setup Git and clone this repository
 
 git clone https://github.com/jisosomppi/mgmt
 
@@ -10,6 +13,8 @@ git config --global user.name "Jussi Isosomppi"
 git config --global user.email "jussi.isosomppi@gmail.com"
 git config --global credential.helper "cache --timeout=3600"
 git config --global push.default simple
+
+# Make Git commit script usable
 
 sudo cp mgmt/gitup /usr/local/bin/
 sudo chmod 755 /usr/local/bin/gitup
