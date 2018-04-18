202fc69bb4472ccf02edd2aac10e869ebaa5874e
Jussi Isosomppi <jussi.isosomppi@gmail.com>
2018-04-18 05:37:05 +0000

    added pillar

--- spreadsalt.sh
+++ spreadsalt.sh
@@ -1,5 +1,6 @@
 sudo cp -avr ~/mgmt/srvsalt/. /srv/salt/
 sudo cp -avr ~/mgmt/etcsalt/. /etc/salt/
+sudo cp -avr ~/mgmt/srvpillar/. /srv/pillar/
 
 sudo systemctl restart salt-minion
 sudo systemctl restart salt-master
