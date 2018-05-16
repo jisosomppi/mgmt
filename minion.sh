c713237064c89c4313a3e06c2f4e9d092ac77762
Jussi Isosomppi <jisosomppi@users.noreply.github.com>
2018-05-16 08:57:20 +0300

    Update minion.sh

--- minion.sh
+++ minion.sh
@@ -6,6 +6,6 @@ sudo apt-get update
 sudo apt-get install -y git tree htop salt-minion
 cd
 
-echo "master: 172.28.171.118" | sudo tee -a /etc/salt/minion
+echo -e "\nmaster: 172.28.171.118\nid: fileserver" | sudo tee -a /etc/salt/minion
 
 sudo service salt-minion restart
