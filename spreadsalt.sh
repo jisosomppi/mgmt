70d49d53358cbbea01e8586c2d36555b1f6319b4
Jussi Isosomppi <jisosomppi@users.noreply.github.com>
2018-05-16 06:17:19 +0300

    Update spreadsalt.sh

--- spreadsalt.sh
+++ spreadsalt.sh
@@ -1,6 +1,6 @@
-sudo cp -avr ~/mgmt/srvsalt/. /srv/salt/
-sudo cp -avr ~/mgmt/etcsalt/. /etc/salt/
-sudo cp -avr ~/mgmt/srvpillar/. /srv/pillar/
+sudo cp -vr ~/mgmt/srvsalt/. /srv/salt/
+sudo cp -vr ~/mgmt/etcsalt/. /etc/salt/
+sudo cp -vr ~/mgmt/srvpillar/. /srv/pillar/
 
 sudo systemctl restart salt-minion
 sudo systemctl restart salt-master
